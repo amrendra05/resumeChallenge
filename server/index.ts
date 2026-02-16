16275b576678d04c7b395fc315b871e4c37e7718
Amrendra <akamrendrak@gmail.com>
2026-02-16 18:45:35 +0100

    removed unwanted lines

--- server/index.ts
+++ server/index.ts
@@ -22,8 +22,6 @@ export async function getSecret(): Promise<string> {
   const client = new SecretManagerServiceClient();
 
   const projectId = await client.getProjectId();
-  //log(`Getting secret for project: ${projectId}`);
-  //const dbURISecretName = process.env.DBURISECRETNAME;
 
   // If config is missing, skip GCP entirely
   if (!projectId) {
